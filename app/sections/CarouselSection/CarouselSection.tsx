b9608917518cfba436f40f86e2376761015e1fc7
M3RCURRRY <m3rcurrry1337@gmail.com>
2026-02-24 09:19:36 +0300

    fix: carousel aspect ratio & new images

--- app/sections/CarouselSection/CarouselSection.tsx
+++ app/sections/CarouselSection/CarouselSection.tsx
@@ -18,6 +18,66 @@ const SLIDES = [
     image: "slide4.png",
     position: "center",
   },
+  {
+    image: "slide5.png",
+    position: "center",
+  },
+  {
+    image: "slide6.png",
+    position: "center",
+  },
+  {
+    image: "slide7.png",
+    position: "center",
+  },
+  {
+    image: "slide8.png",
+    position: "center",
+  },
+  {
+    image: "slide9.png",
+    position: "center",
+  },
+  {
+    image: "slide10.png",
+    position: "center",
+  },
+  {
+    image: "slide11.png",
+    position: "center",
+  },
+  {
+    image: "slide12.png",
+    position: "center",
+  },
+  {
+    image: "slide13.png",
+    position: "center",
+  },
+  {
+    image: "slide14.png",
+    position: "center",
+  },
+  {
+    image: "slide15.png",
+    position: "center",
+  },
+  {
+    image: "slide16.png",
+    position: "center",
+  },
+  {
+    image: "slide17.png",
+    position: "center",
+  },
+  {
+    image: "slide18.png",
+    position: "center",
+  },
+  {
+    image: "slide19.png",
+    position: "center",
+  },
 ];
 
 export default function CarouselSection() {
